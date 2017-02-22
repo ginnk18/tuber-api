91dd69fd2adf74bceef229d0e14dfd4f2f7718a2
Scott Veale <srveale42@gmail.com>
2017-02-22 22:40:58 +0000

    index routed to tutors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'tutors#index'
+
   resources :tutors
   resources :students
   resources :reviews
   resources :subjects
   resources :users
+
+  root to: 'tutors#index'
 
 end
